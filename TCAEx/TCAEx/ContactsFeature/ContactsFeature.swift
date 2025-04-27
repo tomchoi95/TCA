fe34d7dc76662d90064450a1d8a43f5f36e22156
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-27 15:10:24 +0900

    id를 통한 context 제거 기능 추가.

--- TCAEx/TCAEx/ContactsFeature/ContactsFeature.swift
+++ TCAEx/TCAEx/ContactsFeature/ContactsFeature.swift
@@ -34,9 +34,7 @@ struct ContactsFeature {
     }
     
     var body: some ReducerOf<Self> {
-        Reduce {
-            state,
-            action in
+        Reduce { state, action in
             switch action {
                 case .addButtonTapped:
                     // 이제 액션을 추가할 것인데, 이 액션은 위의 add Contact 상태의 옵셔널을 해제하기 만듦
@@ -65,12 +63,18 @@ struct ContactsFeature {
                     )
                     return .none
                     
+                case .alert(.presented(.confirmation(id: let id))):
+                    state.contacts.remove(id: id)
+                    return .none
                     
+                case .alert:
+                    return .none
             }
         }
         // ifLet 이거 냄새가 옵셔널 까는데 쓰는냄새가 남. 즉, 위에서 프레젠테이션 상태가 옵셔널임. 이게 옵셔널이 해제될 때 작동할것임.
         // 그리고 이건 AddContactFeature 리듀셔를 리턴함.
         // 아래는 addContact가 nill이 아닐 경우 그 상태와. action을 가지는 AddContactFeture Reducer 생성.
         .ifLet(\.$addContact, action: \.addContact) { AddContactFeature() }
+        .ifLet(\.$alert, action: \.alert)
     }
 }
