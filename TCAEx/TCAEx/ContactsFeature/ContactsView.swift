19b47589d8fd0c40f8dd084df3e4ac5e14d5975a
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-27 16:14:44 +0900

    삭제 버튼 추가.

--- TCAEx/TCAEx/ContactsFeature/ContactsView.swift
+++ TCAEx/TCAEx/ContactsFeature/ContactsView.swift
@@ -13,34 +13,42 @@ struct ContactsView: View {
     
     var body: some View {
         List(store.contacts) { contact in
-            Text(contact.name)
+            HStack {
+                Text(contact.name)
+                Spacer()
+                Button {
+                    store.send(.deleteButtonTapped(id: contact.id))
+                } label: {
+                    Image(systemName: "trash")
+                        .foregroundColor(.red)
+                }
+            }
         }
         .navigationTitle("Contacts")
-        .toolbar {
-            Button(action: { store.send(.addButtonTapped) }, label: { Image(systemName: "plus") })
-        }
+        .toolbar { Button(action: { store.send(.addButtonTapped) }, label: { Image(systemName: "plus") }) }
         .sheet(
             // 여기서 item이 nil이면 sheet가 닫히고, nill이 아니게 되면, sheet가 열림.
             item: $store.scope(state: \.addContact, action: \.addContact),
-//            그리고 그 열리는 sheet는 { store in AddContactView(store: store) }라는 closure로 구성됨.
-//            또한 그 클로저의 파라메터는 위에서 알 수 있듯이. scope로, state는 addContact, action은 addContact인 store가 됨.
+            //            그리고 그 열리는 sheet는 { store in AddContactView(store: store) }라는 closure로 구성됨.
+            //            또한 그 클로저의 파라메터는 위에서 알 수 있듯이. scope로, state는 addContact, action은 addContact인 store가 됨.
             content: { store in NavigationStack { AddContactView(store: store) } }
         )
+        .alert($store.scope(state: \.alert, action: \.alert))
     }
 }
 
 #Preview {
-        ContactsView(
-            store: Store(
-                initialState: ContactsFeature.State(
-                    contacts: [
-                        Contact(id: UUID(), name: "Blob"),
-                        Contact(id: UUID(), name: "Blob Jr"),
-                        Contact(id: UUID(), name: "Blob Sr"),
-                    ]
-                )
-            ) {
-                ContactsFeature()
-            }
-        )
+    ContactsView(
+        store: Store(
+            initialState: ContactsFeature.State(
+                contacts: [
+                    Contact(id: UUID(), name: "Blob"),
+                    Contact(id: UUID(), name: "Blob Jr"),
+                    Contact(id: UUID(), name: "Blob Sr"),
+                ]
+            )
+        ) {
+            ContactsFeature()
+        }
+    )
 }
